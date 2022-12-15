4510b3c2638c9ad5226006680b67ec0dccb76cc5
Bishal Regmi <bishalregmi@pop-os.localdomain>
2022-12-15 11:57:29 +0545

    Using Ref instead Of State

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
+import React, { useState,useRef,MutableRefObject } from 'react';
 import './App.css';
 import InputField from './Components/InputField';
+import { Task } from './Components/Model';
 
 const App:React.FC = () => {
+  console.log('App Rendering');
+  const taskRef = useRef() as MutableRefObject<HTMLInputElement>;
+  const [tasks,setTasks] = useState<Task[]>([]);
+
+  const handleAdd = (e:React.FormEvent,task:string) =>{
+      e.preventDefault();
+      if(!taskRef.current.value){
+        window.alert('Please Add Task')
+        return;
+      };
+      taskRef.current.value = "";
+      console.log(task);
+      console.log('Current Value',taskRef.current.value)
+  }
   return (
     <div className='App'>
       <span className='heading'>Task Manager</span>
-      <InputField />
+      <InputField handleAdd={handleAdd} taskRef={taskRef}/>
     </div>
   )
 }
--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -1,18 +1,19 @@
-import React,{useRef} from 'react';
+import React,{MutableRefObject, useRef} from 'react';
 import './styles.css';
 
-const InputField = () => {
-    const taskRef = useRef<HTMLInputElement>();
-    function submitHandler(e:any){
-        e.preventDefault();
-        window.alert(`ubmitted ${taskRef.current?.value}`);
-    }
+interface PropsForInputField{
+  handleAdd:(e:React.FormEvent,task:string) => void;
+  taskRef:MutableRefObject<HTMLInputElement>
+}
+
+const InputField = ({handleAdd,taskRef}:PropsForInputField) => {
+  console.log('Input Rendering');
   return (
-    <form className='input' onSubmit={submitHandler}>
+    <form className='input' onSubmit={(e)=>handleAdd(e,taskRef.current.value)}>
         <input type='input' placeholder='Enter Your Task' className='inputBox' ref={taskRef}/>
         <button className='inputSubmit' type='submit'>Add Task</button>
     </form>
   )
 }
 
-export default InputField;
+export default React.memo(InputField);
